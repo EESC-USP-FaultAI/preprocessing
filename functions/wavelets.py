39f6d8438948dfabe3f5ef566dea53880936cfa9
Caio Vinicius <grilocaio@hotmail.com>
2023-10-29 19:32:11 -0300

    Adicionado algumas funções como wavelet morlet e symlet

--- functions/wavelets.py
+++ functions/wavelets.py
@@ -1,1 +1,25 @@
 import numpy as np
+
+#TODO: Fazer mudanças nessa classe
+
+def morlet_wavelet(t, freq, sigma):
+    return np.exp(-t**2 / (2 * sigma**2)) * np.exp(2j * np.pi * freq * t)
+
+def symlet_wavelet(t, j, k):
+
+    #Em que t = tempo
+    # j parâmetro de escala
+    # k parâmetro de translação
+
+    # A função será zero caso tempo negativo
+    if t < 0:
+        return 0
+
+    if t < j - k:
+        return np.exp(-t / 2) * np.sin(2 * np.pi * t)
+
+    if t < j + k:
+        return np.exp(-t / 2) * (np.sin(2 * np.pi * t) + np.cos(2 * np.pi * t))
+
+    # A função será zero para t maior que j + k.
+    return 0
