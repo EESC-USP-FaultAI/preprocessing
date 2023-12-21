89cbfa3783dee198da535a0b0a75a05feb827fb6
Caio Vinicius <grilocaio@hotmail.com>
2023-12-21 18:15:08 -0300

    Wavelet com função implementado

--- functions/waveletTransform.py
+++ functions/waveletTransform.py
@@ -35,6 +35,42 @@ class WaveletTransform:
                 print(wave_name)
         return filter_wavelets
 
+    @staticmethod
+    def dwt(data, wavelet, mode='symmetric', axis=-1, level=1, details=True):
+
+        """
+        :param data: Input signal
+        :param wavelet: Mother wavelet to use
+        :param mode: Signal extension mode (ex: symmetric)
+        :param axis: Axis over which to compute the DWT. If not given, the
+        :param level: Multi-level analysis resolution
+        :param details: Uses details values to get next level, if false is uses analysis signal
+        last axis is used.
+        :return: Approximation and detail the first level coefficients.
+        """
+
+        if level == 1:
+            return pywt.dwt(data, wavelet, mode, axis)
+        elif level > 1:
+
+            (cA, cD) = pywt.dwt(data, wavelet, mode, axis)
+
+            for i in range(1,level):
+                if details:
+                    a1, d1 = pywt.dwt(cD, wavelet, mode, axis)
+                else:
+                    a1, d1 = pywt.dwt(cA, wavelet, mode, axis)
+
+                cA = a1
+                cD = d1
+
+            return cA, cD
+        else:
+            print('Error: Minimum signal level is 1.')
+            return None
+
+
+
 
 a = WaveletTransform
 teste = a.wavelet(display=False)
