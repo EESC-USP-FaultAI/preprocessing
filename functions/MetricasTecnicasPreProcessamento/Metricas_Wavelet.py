7bf7dc375ce9a9ae329bafd0e551c158d9627940
Caio Vinicius <grilocaio@hotmail.com>
2024-02-13 10:26:06 -0300

    Adicionado metricas - inicio v1

---
+++ functions/MetricasTecnicasPreProcessamento/Metricas_Wavelet.py
@@ -0,0 +1,24 @@
+'''
+Metricas de preprocessamentop para wavelet
+'''
+
+
+
+'''
+Análise da energia do sinal atraves de tal e tal
+'''
+
+
+'''
+Função relacionada ao desvio padrão
+'''
+
+
+'''
+Função para calcular a entropia do sinal
+'''
+
+
+'''
+Funções para calcular a energia e a densidade de energia
+'''
