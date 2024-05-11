ee62b823a567bdc0ead480a50c3f152df340e097
gabbenunes <136460461+gabbenunes@users.noreply.github.com>
2024-05-11 15:43:07 -0300

    EvaluatingtheCode

--- functions/TS/AvaliandoProcessamento_v1.py
+++ functions/TS/AvaliandoProcessamento_v1.py
@@ -1,44 +1,43 @@
 
 """
-Código para avaliar o tempo de processamento de sinais
-Será utilizado o sinal gerado com harmônicas
+Code to evaluate the ST of signals with harmonics 
 """
 
-from functions.SignalGenerator.GeraSinais import GeraSinais  # Import the class from the module
+from functions.SignalGenerator.GeraSinais import Generate_Signals  # Import the class from the module
 from functions.TS.ST import Stockwell
 import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Passo 1: Gera o sinal sintético
-amplitude_fundamental = 10  # amplitude da fundamental
+# Step 1: Generate the signals 
+amplitude_fundamental = 10  # Fundamental amplitude 
 samples_per_cycle = 128
-frequency = 60.0  # frequencia em Hz
+frequency = 60.0  # frequency
 harmonics = [2, 3, 5]
-amplitudes = [5, 3, 1]  # Adicione amplitudes correspondentes às harmônicas
+amplitudes = [5, 3, 1]  # Add the amplitude of the harmonics
 duration = 0.2
-harmonics_start_time = 0.1  # tempo em segundos para começar a adicionar as harmônicas
+harmonics_start_time = 0.1  # Time to start to add the harmonics 
 add_noise_harmonics = False  # Change this to True if you want to add noise
 SNR_harmonics = 15  # Change this to set the SNR for the harmonics signal
 
-tempo_sinal, signal = GeraSinais.GenerateSignalWithHarmonics(
+tempo_sinal, signal = Generate_Signals.GenerateSignalWithHarmonics(
     amplitude_fundamental, samples_per_cycle, frequency, harmonics, amplitudes, duration, harmonics_start_time, add_noise_harmonics, SNR_harmonics
 )
 
-# Passo 2: Avalia o tempo de processamrnto do sinal
+# Step 2: Evaluate the signal processing
 # Criar uma instância da classe Stockwell
 stockwell_instance = Stockwell()
-# Chamar o método calcula_TS_do_sinal da instância stockwell_instance
+# Call the method calculate_ST_of_the_signal da instância stockwell_instance
 start_time = time.time()
-amp, ang = stockwell_instance.calcula_TS_do_sinal(signal, samples_per_cycle, 3)
+amp, ang = stockwell_instance.calculate_ST_of_the_signal(signal, samples_per_cycle, 3)
 end_time = time.time()
 tempo_total = end_time - start_time
 
-# plot a linha 1 (fundamental) e as linhas onde tiverem as harmônicas
-# Número de harmônicas
+# plot line 1 (fundamental) and the lines that contain the harmonics
+# Number of harmonics
 num_harm = len(harmonics) + 1
 
-# Criar subplots para 'amp'
+# Create subplots for 'amp'
 plt.figure(figsize=(12, 8))
 j=0
 for i in [1] + harmonics:
@@ -51,7 +50,7 @@ for i in [1] + harmonics:
     plt.legend()
     j+=1
 
-plt.suptitle(f'Tempo de Processamento: {tempo_total:.4f} segundos', y=1.02)
+plt.suptitle(f'Processing Time: {tempo_total:.4f} segundos', y=1.02)
 plt.tight_layout()
 plt.show()
 
