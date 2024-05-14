c9d3801b4eedf21ceaa5276745181c672fe58ea9
Leonardo Lessa <leonardolessa@usp.br>
2024-05-14 09:14:36 -0300

    comentários das linhas de código

--- functions/EMD/EMD.py
+++ functions/EMD/EMD.py
@@ -1,59 +1,62 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import emd
+#Código comentado linha a linha em inglês
 
-from functions.SignalGenerator.SignalGenerator import GeraSinais # Import the class from the module
+import numpy as np  # Import the NumPy library for numerical operations
+import matplotlib.pyplot as plt  # Import the Matplotlib library for plotting graphs
+import emd  # Import the emd package for Empirical Mode Decomposition analysis
 
+from GeraSinais import GeraSinais  # Import the GeraSinais class from the GeraSinais module
 
 # Example usage
-sag_magnitude = 0.5  # Change this to set the sag magnitude
-start_time = 0.1  # Change this to set the start time of the sag
-end_time = 0.5  # Change this to set the end time of the sag
-duration = 1.0  # Change this to set the duration of the sag
-samples_per_cycle = 128
-frequency = 60.0  # frequencia em Hz
-sampling_frequency = samples_per_cycle * frequency  # Change this to set the sampling frequency
-involved_phases = 'AB'  # Change this to set the involved phase or combination
+sag_magnitude = 0.5  # Magnitude of the voltage sag
+start_time = 0.1  # Start time of the voltage sag
+end_time = 0.5  # End time of the voltage sag
+duration = 1.0  # Total duration of the signal
+samples_per_cycle = 128  # Number of samples per cycle
+frequency = 60.0  # Frequency of the electrical network in Hz
+sampling_frequency = samples_per_cycle * frequency  # Sampling frequency
+involved_phases = 'AB'  # Involved phases
 
 # Example usage with noise
-add_noise_sag = False  # Change this to True if you want to add noise
-SNR_sag = 20  # Change this to set the SNR for the sag signal
+add_noise_sag = True  # Add noise to the voltage sag signal
+SNR_sag = 20  # Signal-to-noise ratio for the voltage sag signal
 
+# Generate the three-phase voltage sag signal caused by short circuit
 resulting_voltages = GeraSinais.voltage_sag_short_circuit(
     sag_magnitude, start_time, end_time, duration, sampling_frequency, involved_phases, add_noise_sag, SNR_sag
 )
 
 # Plotting
+'''
 for phase, voltage in resulting_voltages.items():
     plt.plot(np.arange(0, duration, 1 / sampling_frequency), voltage, label=f'Phase {phase}')
+'''
 
-'''plt.title('Three-Phase Voltage Sag caused by Short Circuit')
+'''
+plt.title('Three-Phase Voltage Sag caused by Short Circuit')
 plt.xlabel('Time (s)')
 plt.ylabel('Voltage (pu)')
 plt.legend()
 plt.grid(True)
-plt.show()'''
+plt.show()
+'''
 
+# Convert the resulting voltage signal into a NumPy array
 signal = np.array(resulting_voltages['A'])
+
+# Create a time vector
 time = np.arange(0, duration, 1 / sampling_frequency)
+
+# Perform Empirical Mode Decomposition (EMD) of the signal
 imf = emd.sift.sift(signal)
 
+# Calculate the Hilbert Transform
 IP, IF, IA = emd.spectra.frequency_transform(imf, sampling_frequency, 'hilbert')
 
-freq_range = (0, 10, 100)  # 0 to 10Hz in 50 steps
-f, hht = emd.spectra.hilberthuang(IF, IA, freq_range, sum_time=False)
+# Plot the IMF (Intrinsic Mode Functions) modes
+emd.plotting.plot_imfs(imf)
 
-plt.figure(figsize=(10, 8))
-plt.subplot(211, frameon=False)
-plt.plot(time, signal, 'k')
-plt.plot(time, imf[:, 0]-4, 'r')
-plt.plot(time, imf[:, 1]-8, 'g')
-plt.plot(time, imf[:, 2]-12, 'b')
-plt.xlim(time[0], time[-1])
-plt.grid(True)
-plt.subplot(212)
-plt.pcolormesh(time, f, hht, cmap='ocean_r')
-plt.ylabel('Frequency (Hz)')
-plt.xlabel('Time (secs)')
-plt.grid(True)
+# Adjust the layout of the plot
+plt.tight_layout()
+
+# Display the plot
 plt.show()
