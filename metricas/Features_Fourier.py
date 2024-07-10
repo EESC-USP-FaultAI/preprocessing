71553ba6d314d158ffb3a06865c3ff647ecf5c6f
Alailton-Jr <alailtonjunior@usp.br>
2024-07-10 08:42:52 -0300

    Fourier features updated

--- metricas/Features_Fourier.py
+++ metricas/Features_Fourier.py
@@ -1,10 +1,115 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 ## Include the directory of the project in the path to import the "functions"
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import functions.TF as tf
+
+# Comparative Analysis of DFT - Based Synchrophasor Estimators
+    # Comparison of diferent DFT variants for synchrophasor estimation
+    
+sym_matrix = np.array([
+    [1.0/3.0, 1.0/3.0, 1.0/3.0],
+    [1.0/3.0, -0.5/3.0 - 0.86602540378/3.0*1j, -0.5/3.0 + 0.86602540378/3.0*1j],
+    [1.0/3.0, -0.5/3.0 + 0.86602540378/3.0*1j, -0.5/3.0 - 0.86602540378/3.0*1j]
+])
+
+def first_paper_features(data_fft):
+    '''
+        Based Papaer: 
+            Fourier Transform and Probabilistic Neural Network based Fault Detection in Distribution System Containing DGs
+
+        Description:
+            This paper used the DFT to classify the fault in the distribution system. The algorithm used by the author estimates the magnitude and angle of the phasors using the Modified Full Cycle Discrete Fourier Transform (MFCDFT). After that, the author calculates the positive sequence component model matrix from the estimated phasors. 
+
+        The features extracted from the paper are the following:
+            - i_1: Positive sequence component
+
+        Additional features outside of the paper:
+            - i_0: Zero sequence component
+            - i_2: Negative sequence component
+    '''
+    i012 = np.abs(np.einsum('ij, jk->ik', sym_matrix, data_fft[:,:,1]))
+    F1 = np.concatenate([i012.mean(axis=-1), i012.std(axis=-1), statistical_values(i012)])
+    return F1
+
+def second_paper_feature(data_fft):
+    '''
+    Based Papaer:
+        Modified FFT based high impedance fault detection technique considering distribution non-linear loads: Simulation and experimental data analysis
+    
+    Description:
+        This paper uses the energy of the harmonic components to detect high impedance faults
+
+    The following features are calculated:
+        - h2: Harmonic 2
+        - h3: Harmonic 3
+        - h5: Harmonic 5
+        - h7: Harmonic 7
+        - h5/h3: Ratio of Harmonic 5 and Harmonic 3
+        - h7/h3: Ratio of Harmonic 7 and Harmonic 3
+        - (h7+h5)/h1: Ratio of Harmonic 7 and Harmonic 5
+    '''
+    h1 = np.mean(data_fft[:, :, 1], axis=-1)
+    h2 = np.mean(data_fft[:, :, 2], axis=-1)
+    h3 = np.mean(data_fft[:, :, 3], axis=-1)
+    h5 = np.mean(data_fft[:, :, 5], axis=-1)
+    h7 = np.mean(data_fft[:, :, 7], axis=-1)
+    return np.concatenate([h2, h3, h5, h7, h5/h3, h7/h3, (h7+h5)/h1])
+
+def third_paper_feature(data_fft, data_fft_angle, angRef_pos:int=0):
+    '''
+    Based Papaer:
+        Fault Detection on Distribution Network Planning Using Fast Fourier Transform-Based Steady State and Transient Response
+    
+    Description:
+        Fault classifier using maximum voltage and it's angle
+
+    The following features are calculated:
+        - Vmax: Maximum voltage of each phase
+        - AngVmax: Angle of the maximum voltage of each phase
+    '''
+    
+    idx_max = np.argmax(data_fft[:,:,1], axis=-1)
+    v_max = data_fft[np.arange(data_fft.shape[0]), idx_max,1]
+    ang_max = data_fft_angle[np.arange(data_fft.shape[0]), idx_max,1] - np.angle(data_fft[angRef_pos, idx_max[angRef_pos],1])
+    return np.concatenate([v_max, ang_max])
+
+def fourth_paper_feature(data_fft):
+    '''
+    Based Papaer:
+        Frequency Estimation of Distorted and Noisy Signals in Power Systems by FFT-Based Approach
+
+    Description:
+        Frequency estimation of distorted and noisy signals in power systems by FFT-based approach
+
+    The following features are calculated:
+    '''
+    #
+    # beta = (|X[k2]| - |X[k1]|) / (|X[k2]| + |X[k1]|)
+    beta = (data_fft[:, 1:, 0] - data_fft[:, :-1, 0]) / (data_fft[:, 1:, 0] + data_fft[:, :-1, 0])
+
+def statistical_values(data):
+
+    if len(data.shape) < 3:
+        while len(data.shape) < 3:
+            data = data[np.newaxis, ...]
+
+    centroid_all = np.sum(data[:, :, :] * np.arange(data.shape[2]), axis=-1) / np.sum(data[:, :, :], axis=-1)
+    centroid = np.mean(centroid_all, axis=-1)
+
+    variance_all = np.var(data, axis=-1)
+    variance = np.mean(variance_all, axis=-1)
+
+    skewness_all = np.mean((data - centroid_all[:, :, np.newaxis])**3, axis=-1) / variance_all**1.5
+    skewness = np.mean(skewness_all, axis=-1)
+
+    kurtosis_all = np.mean((data - centroid_all[:, :, np.newaxis])**4, axis=-1) / variance_all**2
+    kurtosis = np.mean(kurtosis_all, axis=-1)
+
+    return np.concatenate([centroid, variance, skewness, kurtosis])
 
 def dft_features(data, fs:int, base_freq:int=60):
     """
@@ -31,34 +136,59 @@ def dft_features(data, fs:int, base_freq:int=60):
     slides = np.lib.stride_tricks.sliding_window_view(data, window_shape=N, axis=-1)
     data_fft = np.fft.fft(slides, axis=-1)
     # data_fft = np.apply_along_axis(tf.FFT, -1, slides)
+    data_fft_angle = np.angle(data_fft[:,:,:N//2])
     data_fft = np.abs(data_fft[:,:,:N//2]) / (N//2)
 
-    h1 = np.mean(data_fft[:, :, 1], axis=-1)
-    h2 = np.mean(data_fft[:, :, 2], axis=-1)
-    h3 = np.mean(data_fft[:, :, 3], axis=-1)
-    h5 = np.mean(data_fft[:, :, 5], axis=-1)
-    h7 = np.mean(data_fft[:, :, 7], axis=-1)
+    if len(data.shape) > 3:
+        # The Paper uses Symetrical Components, it needs the value of the 3 phase current
+        # I'm supposing that the 3 phase current is the first 3 channels
+        F1 = first_paper_features(data_fft[:3])
+    else: 
+        F1 = np.zeros((1,))
 
-    centroid_all = np.sum(data_fft[:, :, :] * np.arange(N//2), axis=-1) / np.sum(data_fft[:, :, :], axis=-1)
-    centroid = np.mean(centroid_all, axis=-1)
+    F2 = second_paper_feature(data_fft)
 
-    variance_all = np.var(data_fft, axis=-1)
-    variance = np.mean(variance_all, axis=-1)
+    if len(data.shape) == 6:
+        # The Paper uses the maximum voltage and it's angle
+        # I'm supposing that the voltage is the last 3 channels
+        F3 = third_paper_feature(data_fft[3:], data_fft_angle[3:], 0)
+    else:
+        F3 = np.zeros((1,))
+    F4 = statistical_values(data_fft)
 
-    skewness_all = np.mean((data_fft - centroid_all[:, :, np.newaxis])**3, axis=-1) / variance_all**1.5
-    skewness = np.mean(skewness_all, axis=-1)
 
-    kurtosis_all = np.mean((data_fft - centroid_all[:, :, np.newaxis])**4, axis=-1) / variance_all**2
-    kurtosis = np.mean(kurtosis_all, axis=-1)
-
-    return np.concatenate([h2, h3, h5, h7, h5/h3, h7/h3, (h7+h5)/h1, centroid, variance, skewness, kurtosis])
+    return np.concatenate([F1,F2,F3,F4])
 
 
 if __name__ == '__main__':
     df = pd.read_csv(r'C:\Users\alail\Downloads\dados_testes\configuracao_2\corrente\dados_cru_corrente_config_2.csv', skiprows=1)
     signal = df.to_numpy()
-    signal = signal[:, 1:4]
-
-    features = dft_features(signal[:,0], 30720, 60)
+    signal = signal[:, 1:4].T
+    df = pd.read_csv(r'C:\Users\alail\Downloads\dados_testes\configuracao_2\tensao\dados_cru_tensao_config_2.csv', skiprows=1)
+    signal2 = df.to_numpy()
+    signal2 = signal2[:, 1:4].T
+    signal = np.concatenate([signal, signal2], axis=0)
+    features = dft_features(signal, 30720, 60)
 
     print(features)
+
+    # x = First_Featured_Based_Paper(signal[:,:3].T, 30720, 60)
+
+    # plt.subplot(311)
+    # plt.plot(signal[:,0])
+    # plt.subplot(312)
+    # plt.plot(signal[:,1])
+    # plt.subplot(313)
+    # plt.plot(signal[:,2])
+    # plt.show()
+
+    # plt.subplot(311)
+    # plt.plot(np.abs(x[0,:]))
+    # plt.subplot(312)
+    # plt.plot(np.abs(x[1,:]))
+    # plt.subplot(313)
+    # plt.plot(np.abs(x[2,:]))
+    # plt.show()
+
+
+
